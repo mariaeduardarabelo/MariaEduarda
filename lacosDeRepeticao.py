aaee6461d9c255e3d36973d6e7ef164e336a352e
Maria Eduarda <mariaeduardasantosrabelo25@gmail.com>
2024-09-28 14:33:21 -0300

    Atualização

--- lacosDeRepeticao.py
+++ lacosDeRepeticao.py
@@ -5,20 +5,7 @@
 #    numero += 1
 
 
-# numero = 0
-# while numero <=1000:
-#     print(f"{numero}")
-#     numero += 2
-
-
-# numero = 0
-# while numero <=10:
-#     resultado = 2 * numero
-#     print(f"2 x {numero} = {resultado}")
-#     numero += 1
-
 numero = 0
-tabuada = int(input("Qual a tabuada desejada?"))
-while numero <= 10:
-    print(f"{tabuada} * {numero} = {numero * tabuada}")
-    numero += 1
+while numero <=1000:
+    print(f"{numero}")
+    numero += 2
---
+++ tabuada.py
@@ -0,0 +1,11 @@
+# numero = 0
+# while numero <=10:
+#     resultado = 2 * numero
+#     print(f"2 x {numero} = {resultado}")
+#     numero += 1
+
+numero = 0
+tabuada = int(input("Qual a tabuada desejada?"))
+while numero <= 10:
+    print(f"{tabuada} * {numero} = {numero * tabuada}")
+    numero += 1
